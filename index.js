2cf107184ffdaf8f67b59366a330c55d3690470f
Arnav Luthra <arnavl105@gmail.com>
2023-09-19 11:58:40 +0900

    cleaning up tests and console logs

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-var testInput = `7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-22 13 17 11 0
-8 2 23 4 24
-21 9 14 16 7
-6 10 3 18 5
-1 12 20 15 19`;
-
-var testInput2 = `22,2,14,18,19
-22 13 17 11 0
-8 2 23 4 24
-21 9 14 16 7
-6 10 3 18 5
-1 12 20 15 19`;
-
-var testInput3 = `22,2,14,18,19
-22 13 17 11 0
-2 8 23 4 24
-14 21 9 16 7
-18 10 3 18 5
-19 12 20 15 19`;
-
-var testInput4 = `0,4,14,10,1
-22 13 17 11 0
-8 2 23 4 24
-21 9 14 16 7
-6 10 3 18 5
-1 12 20 15 19`;
-
 const crossOutBoard = (board, numbers) => {
 	//make a board of zeros
 	const newBoard = board.map(row => row.map(cell => 0));
@@ -75,15 +47,12 @@ const main = (input) => {
 
 	const lines = input.split('\n');
 	const [numbers, ...board] = lines;
+
 	const calledNumbersArray = numbers.split(',').map(Number);
 	const bingoBoard = board.map(line => line.split(' ').map(Number));
-
 	const crossedOutBoard = crossOutBoard(bingoBoard, calledNumbersArray);
 
 	return isBingo(crossedOutBoard)
 };
 
-console.log(main(testInput));
-console.log(main(testInput2));
-console.log(main(testInput3));
-console.log(main(testInput4));
+module.exports = main;
--- index.test.js
+++ index.test.js
@@ -1,28 +1,57 @@
 const main = require('./index.js');
 
-test('Test case 1', () => {
+test('Base case', () => {
   const input = `
-    7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
-    22 13 17 11 0
-    8 2 23 4 24
-    21 9 14 16 7
-    6 10 3 18 5
-    1 12 20 15 19
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+22 13 17 11 0
+8 2 23 4 24
+21 9 14 16 7
+6 10 3 18 5
+1 12 20 15 19
   `;
-  expect(main(input)).toBe(true);  // Adjust the expected output accordingly
+  expect(main(input)).toBe(true);
 });
 
-test('Test case 2', () => {
-  const input = `
-    22,2,14,18,19
-    22 13 17 11 0
-    8 2 23 4 24
-    21 9 14 16 7
-    6 10 3 18 5
-    1 12 20 15 19
-  `;
-  expect(main(input)).toBe(false); // Adjust the expected output accordingly
+test('Diagonal Bingo', () => {
+    var input = `22,2,14,18,19
+22 13 17 11 0
+8 2 23 4 24
+21 9 14 16 7
+6 10 3 18 5
+1 12 20 15 19`;
+
+  expect(main(input)).toBe(true);
 });
 
-// ... (add more test cases following the same structure)
+test('Reverse Diagonal Bingo', () => {
+   var input = `0,4,14,10,1
+22 13 17 11 0
+8 2 23 4 24
+21 9 14 16 7
+6 10 3 18 5
+1 12 20 15 19`;
 
+  expect(main(input)).toBe(true);
+});
+
+test('Horiztontal Bingo', () => {
+  var input = `22,2,14,18,19
+22 13 17 11 0
+2 8 23 4 24
+14 21 9 16 7
+18 10 3 18 5
+19 12 20 15 19`;
+
+  expect(main(input)).toBe(true);
+});
+
+test('Vertical Bingo', () => {
+  var input = `22,2,14,18,19
+22 13 17 11 0
+2 8 23 4 24
+14 21 9 16 7
+18 10 3 18 5
+19 12 20 15 19`;
+
+  expect(main(input)).toBe(true);
+});
